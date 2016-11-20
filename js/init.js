84402c5c370aba17eacc229426de2427b0cc87b5
Tim Pender <mrtimpender@gmail.com>
2016-11-20 15:58:59 -0700

    experience shell built, scrollspy, styling added

--- js/init.js
+++ js/init.js
@@ -3,6 +3,9 @@
 
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
+    $('.scrollspy').scrollSpy({
+      scrollOffset: 0
+    });
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
